d88e71554a09e5a1f5adbfb2e80d0b9179ec08a1
Nazar Mozol <xfce4@yandex.ua>
2017-11-06 00:56:02 +0200

    Copy to clipboard

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # pg
+
 Password generation
+
+## How to use
+
+1. Set static keys: *key 1* and *key2* (optional).
+2. Set dynamic key: *key 3* (optional).
+3. Focus out *key 3* field.
+4. Copy generated password to clipboard.
+
+To see a live example, visit [the project page](https://acruxray.github.io/pg).
+
+## Credits
+
+[Clipboard.js](https://clipboardjs.com/)
--- js/main.js
+++ js/main.js
@@ -10,6 +10,35 @@ window.onload = function() {
 
 		document.getElementById('pg').innerHTML = pg(password, salt1, salt2);
 
+		var cb = document.getElementById('clipboard');
+		cb.innerHTML = 'Copy to clipboard';
+		cb.style = 'color: inherit';
+
+		copyToClipboard('clipboard');
+
+	});
+
+}
+
+
+function copyToClipboard(id) {
+
+	var cb = document.getElementById(id);
+
+	var clipboard = new Clipboard('#' + id);
+
+	clipboard.on('success', function(e) {
+		// console.log(e);
+		cb.innerHTML = 'The password has been copied';
+		cb.style = 'color: #00cc00';
+
+		e.clearSelection();
+	});
+
+	clipboard.on('error', function(e) {
+		// console.log(e);
+		cb.innerHTML = 'The password was not copied';
+		cb.style = 'color: #990000';
 	});
 
 }
